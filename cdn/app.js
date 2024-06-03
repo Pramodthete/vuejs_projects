4a95ec99b2597bb57d09166bfa101efb07dbffee
Pramod0836 <pthete31@gmail.com>
2024-06-03 13:03:55 +0530

    ADDED: make demo project and practice some methods and events

---
+++ cdn/app.js
@@ -0,0 +1,7 @@
+console.log("Heloo Vue JS");
+
+const app = Vue.createApp({
+    template:"<h1>Heloo from Vue JS</h1>"
+});
+
+app.mount("#app");
---
+++ demo_project/src/components/HomeVue.vue
@@ -0,0 +1,42 @@
+<template >
+    <h1 v-on:mousemove="getConsole('mouse moved')">Home Component</h1>
+    <h1>Email : {{ email }}</h1>
+    <h1>Mobile : {{ mobile = 9999 }}</h1>
+
+    <h1>Name: {{ getName('Pramod Thete') }}</h1>
+    <h2></h2>
+
+    <button v-on:click="getAlert('Alert Message')">Click Me</button>
+    <button v-on:dblclick="getAlert('Alert Message')">Double Click Me</button>
+    <button v-on:click="getData()">Count</button>
+    <h2>{{ count }}</h2>
+</template>
+
+<script>
+    export default {
+        name:'HomeVue',
+        data(){
+            return {count:0}
+        },
+        methods:{
+            getName(name){
+                return name;
+            },
+            getData(){
+                this.count+=10;
+            },
+            getAlert(name){
+                alert(name)
+            },
+            getConsole(data){
+                console.warn(data);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1{
+    color:orange;
+}
+</style>
